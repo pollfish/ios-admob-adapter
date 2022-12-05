9223205cc97ac83f36ee45d9072b2b4a7368720d
Fotis Mitropoulos <fotis@pollfish.com>
2022-12-05 12:29:49 +0200

    Adding Swift Package Manager Support

---
+++ Package.swift
@@ -0,0 +1,40 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PollfishAdMobAdapter",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "PollfishAdMobAdapter",
+            targets: ["PollfishAdMobAdapter", "Wrapper"])
+    ],
+    dependencies: [
+        .package(
+            name: "GoogleMobileAds",
+            url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
+            "9.0.0" ..< "10.0.0"
+        ),
+        .package(
+            name: "Pollfish",
+            url: "https://github.com/pollfish/ios-sdk-pollfish.git",
+            .exact("6.3.0")
+        )
+    ],
+    targets: [
+        .target(
+            name: "Wrapper",
+            dependencies: ["Pollfish", "GoogleMobileAds"],
+            path: "Wrapper"
+        ),
+        .binaryTarget(
+            name: "PollfishAdMobAdapter",
+            path: "PollfishAdMobAdapter.xcframework"
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
+)
---
+++ Wrapper/dummy.m
@@ -0,0 +1,4 @@
+#import <TargetConditionals.h>
+#if !TARGET_OS_IOS
+#warning "PollfishAdMobAdapter only supports the iOS platform"
+#endif
