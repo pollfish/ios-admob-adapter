4d159a2a3631bb41b28fc718e025435170d1167f
Fotis Mitropoulos <fotis@pollfish.com>
2023-03-07 10:21:48 +0200

    Pollfish iOS AdMob Adapter v6.4.0.0

--- PollfishAdMobAdapter/PollfishAdMobAdapter/GADMAdapterPollfishConstants.h
+++ PollfishAdMobAdapter/PollfishAdMobAdapter/GADMAdapterPollfishConstants.h
@@ -8,6 +8,6 @@
 #define POLLFISH_DEBUG 0
 
 // Adapter version string
-static NSString *const kGADMAdapterPollfishVersion = @"6.3.1.1";
+static NSString *const kGADMAdapterPollfishVersion = @"6.4.0.0";
 
 static NSString *const kGADMAdapterPollfishErrorDomain = @"com.pollfish.mediation";
